0d822e6641640d0e147f841dcde7642f9a0c093f
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-06-19 13:33:15 +0100

    few basic functions created. TODO calculateRisk().

---
+++ Part1/diabetestool.js
@@ -0,0 +1,23 @@
+// function to grab ID for a given element
+function getID(elementId) {
+    return document.getElementById(elementId);
+}
+
+// function to return an array of element with the same name
+function getName(elementName) {
+    return document.getElementsByName(elementName);
+}
+
+// function to grab all the elements
+function getTagElement(elementTagName) {
+    return document.getElementsByTagName(elementTagName);
+}
+
+// function to get element by class name
+function getClassNames(classElements) {
+    return document.getElementsByClassName(classElements);
+}
+
+function calculateRisk() {
+    //TODO
+}
