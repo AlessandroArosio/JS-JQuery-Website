b88295b2412c2129c489295bc1152765bc6a446b
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-06-20 23:15:21 +0100

    First page completed. Result box now shows appropriate message after carrying out risk calculation.
    TODO: contact form in both HTML and JS

--- Part1/diabetestool.js
+++ Part1/diabetestool.js
@@ -1,28 +1,88 @@
-// function to grab ID for a given element
-
-function getID(elementId) {
-    return document.getElementById(elementId);
-}
-
 // function to return an array of element with the same name
 function getName(elementName) {
     return document.getElementsByName(elementName);
 }
 
-// function to grab all the elements
-function getTagElement(elementTagName) {
-    return document.getElementsByTagName(elementTagName);
-}
-
-// function to get element by class name
-function getClassNames(classElements) {
-    return document.getElementsByClassName(classElements);
-}
-
 function calculateRisk() {
-    //TODO
+    var score = getTotalRadioValues();
+    var sumScore = 0;
+    var message;
+
+    // calculating the sum of all the element in the array
+    for (var i = 0; i < score.length; i++){
+        sumScore += score[i];
+    }
+
+    if (sumScore > 25) {
+        message = "Your results show that you currently have a HIGH risk of " +
+            "developing diabetes. " + highRiskMessage() +
+            ". We advise that you contact the Health Authority to " +
+            "discuss your risk factors as soon as you can. Please fill in our " +
+            "<a id='contactForm' href='contactform.html'>contact form</a> and a member of the Health Authority Diabetes " +
+            "Team will be in contact with you.";
+    } else if (sumScore > 15) {
+        message = "Your results show that you currently have a medium risk of " +
+            "developing diabetes. For more information on your risk " +
+            "factors, and what to do about them, please visit our diabetes " +
+            "advice website at <a id='link' href='http://www.zha.org.zd'>here</a>."
+    } else {
+        message = "Your results show that you currently have a low risk of " +
+            "developing diabetes. However, it is important that you " +
+            "maintain a healthy lifestyle in terms of diet and exercise.";
+    }
+    return message;
 }
 
+// customised message for high risk factor
+function highRiskMessage() {
+    var highRiskMessage = "Your main risk factor(s) ";
+    var radioButtonValues = getTotalRadioValues();
+    var riskFactor = [];
+
+    // creating an array of the highest risk factor(s)
+    if (radioButtonValues[0] >= 10) {
+        riskFactor.push("Age");
+    }
+    if (radioButtonValues[1] >= 10) {
+        riskFactor.push("BMI");
+    }
+    if (radioButtonValues[2] >= 10) {
+        riskFactor.push("Family");
+    }
+    if (radioButtonValues[3] >= 10) {
+        riskFactor.push("Diet");
+    }
+
+    // depending on the length of the array, an appropriate message is generated
+    switch (riskFactor.length) {
+        case 1:
+            highRiskMessage += "is your " + riskFactor[0];
+            break;
+        case 2:
+            highRiskMessage += "are " + riskFactor[0] + " and your " + riskFactor[1];
+            break;
+        case 3:
+            highRiskMessage += "are " + riskFactor[0] + ", " + riskFactor[1] + " and your " + riskFactor[2];
+            break;
+        case 4:
+            highRiskMessage += "are " + riskFactor[0] + ", " + riskFactor[1] +
+               ", " + riskFactor[2] + " and your " + riskFactor[3];
+            break;
+    }
+    return highRiskMessage;
+
+}
+
+// function to show the result box with risk factor and relevant message
+function showResult() {
+    var message = calculateRisk();
+    document.getElementById("risk").innerHTML = message;
+
+    var changeAttribute = document.getElementById("result");
+    changeAttribute.setAttribute("class", "resultBox");
+}
+
+// Function to get a single value from a radio button
 function getSingleRadioValue(field){
     var valueOfRadio;
     var fieldName = getName(field);
@@ -30,25 +90,29 @@ function getSingleRadioValue(field){
         if (fieldName[i].checked) {
             valueOfRadio = fieldName[i].value;
             valueOfRadio = parseInt(valueOfRadio);
-            console.log(valueOfRadio);
         }
     }
     return valueOfRadio;
 }
 
+// Function to calculate the sum of all the values from radio buttons
 function getTotalRadioValues(){
-    var totalScore = getSingleRadioValue("age");
-    totalScore += getSingleRadioValue("bmi");
-    totalScore += getSingleRadioValue("family");
-    totalScore += getSingleRadioValue("diet");
-    console.log("Total score is " + totalScore);
+    var ageScore = getSingleRadioValue("age");
+    var bmiScore = getSingleRadioValue("bmi");
+    var familyScore = getSingleRadioValue("family");
+    var dietScore = getSingleRadioValue("diet");
+
+    var totalScore = [];
+    totalScore.push(ageScore, bmiScore, familyScore, dietScore);
     return totalScore;
 }
 
-window.onload = function () {
+function init() {
     document.getElementById("submit").onclick = function (){
-        alert(getTotalRadioValues());
-
+        document.getElementById("submit").disabled = true;
+        showResult();
         return false;
     };
-};
+}
+
+window.onload = init;
