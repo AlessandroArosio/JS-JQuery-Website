7ee0a51a1a30f6d08c627333a75e7b6dc9f10168
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-06-27 00:32:20 +0100

    General refactor to comply to FMA specs. placeholder replaced with dynamic text, submit button of type "submit" now, few styles added in CSS
    TODO:
    1. fix button "Calculate" in diabetestool.html (and same in Part 2)
    1.1 Result box in the same page
    
    2. Validate data in JS and JQ

--- Part1/contactform.js
+++ Part1/contactform.js
@@ -2,9 +2,11 @@ window.onload = init;
 
 function init() {
     setInitialFocus();
-    addPlaceholder("lastname", "e.g. Arosio");
-    checkFocus();
+    textHint("zha", "ZHA123456");
+    textHint("phone", "11 digits only");
     toolTip();
+    // checkErrorMessage("firstname", 2);
+    // checkErrorMessage('lastname', 2);
 }
 
 // this function simply set the focus on the box first name when the page is fully loaded
@@ -16,18 +18,29 @@ function setInitialFocus() {
    @param element : element to be modified
    @param message : message to be displayed in the placeholder
    */
-function addPlaceholder(element, message) {
-    document.getElementById(element).setAttribute("placeholder", message);
+function textHint(element, defaultText) {
+    document.getElementById(element).setAttribute("class", "defaultText");
+    document.getElementById(element).setAttribute("value", defaultText);
+    setDefaultText(element, defaultText);
 }
 
 // this function check the focus on the last name input box. Is that box is active, the placeholder message disappears.
 // If the user leaves the box empty, the message reappears.
-function checkFocus() {
-    document.getElementById("lastname").onfocus = function () {
-        this.setAttribute("placeholder", "");
+function setDefaultText(element, defaultMessage) {
+    document.getElementById(element).onfocus = function () {
+        if (this.value === defaultMessage) {
+            document.getElementById(element).setAttribute("class", "normalText");
+            this.value = "";
+        }
     };
-    document.getElementById("lastname").onblur = function () {
-        this.setAttribute("placeholder", "e.g. Arosio");
+    document.getElementById(element).onblur = function () {
+        if (this.value === defaultMessage) {
+            document.getElementById(element).setAttribute("class", "defaultText");
+        }
+         else if (this.value === "") {
+            document.getElementById(element).setAttribute("class", "defaultText");
+            this.value = defaultMessage;
+        }
     };
 }
 
@@ -41,4 +54,26 @@ function toolTip() {
     document.getElementById("qMark").onmouseout = function () {
         tooltip.setAttribute("class", "hidden");
     };
+}
+
+// function to show an error message if the input box is empty or does not meet the required length.
+/*
+
+ */
+function checkErrorMessage(element, length){
+    document.getElementById(element).onblur = function() {
+        var elementValue = document.getElementById(element).value;
+        var errorMessage = "";
+
+        if (elementValue == ""){
+            errorMessage = "Mandatory field, cannot be left empty";
+        }
+        else if (elementValue.length < length) {
+            errorMessage = "The name is too short. Minimum length 2 characters";
+        }
+        document.getElementById("errorMessage").innerHTML = errorMessage
+    };
+    document.getElementById(element).onfocus = function () {
+        document.getElementById('errorMessage').innerHTML = "";
+    }
 }
