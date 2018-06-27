04f74182017f1e0f2e683d9d806221ed33a0f9ff
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-06-28 00:15:41 +0100

    Refactored some function (error checking), added new functions. Regex added. In short, data is now being validated, specifically the full name, email address and title.
    TODO:
    1. Validate ZHA number and phone number
    2. error messages in contactForm (part 2)

--- Part1/contactform.js
+++ Part1/contactform.js
@@ -5,8 +5,7 @@ function init() {
     textHint("zha", "ZHA123456");
     textHint("phone", "11 digits only");
     toolTip();
-    checkErrorMessage("firstname", 2);
-    // checkErrorMessage('lastname', 2);
+    formErrorsCheck();
 }
 
 // this function simply set the focus on the box first name when the page is fully loaded
@@ -37,7 +36,7 @@ function setDefaultText(element, defaultMessage) {
         if (this.value === defaultMessage) {
             document.getElementById(element).setAttribute("class", "defaultText");
         }
-         else if (this.value === "") {
+        else if (this.value === "") {
             document.getElementById(element).setAttribute("class", "defaultText");
             this.value = defaultMessage;
         }
@@ -56,24 +55,90 @@ function toolTip() {
     };
 }
 
+function formErrorsCheck() {
+    nameErrorMessage('firstname', 2);
+    nameErrorMessage('lastname', 2);
+    validEmail();
+    validTitle();
+}
+
+function validTitle(){
+    var title = document.getElementById("title").value;
+
+    return title != -1;
+}
+
+function validEmail() {
+    document.getElementById('email').onblur = function () {
+        var elementValue = document.getElementById('email').value;
+        var errorMessage = "";
+        var validEmail = validate("email");
+
+        if (elementValue == "") {
+            errorMessage = "Mandatory field, cannot be left empty";
+        }
+        else if (!validEmail) {
+            errorMessage = "Email is incorrect";
+        }
+        var elementTarget = document.getElementById('field_email').getElementsByClassName("errorMessage")[0];
+        elementTarget.innerHTML = errorMessage;
+    };
+    document.getElementById('email').onfocus = function () {
+        var elementTarget = document.getElementById('field_email').getElementsByClassName("errorMessage")[0];
+        elementTarget.innerHTML = "";
+    };
+}
+
 // function to show an error message if the input box is empty or does not meet the required length.
 /*
 
  */
-function checkErrorMessage(element, length){
+function nameErrorMessage(element, length){
     document.getElementById(element).onblur = function() {
         var elementValue = document.getElementById(element).value;
         var errorMessage = "";
+        var validInput = validate(element);
 
         if (elementValue == ""){
             errorMessage = "Mandatory field, cannot be left empty";
         }
         else if (elementValue.length < length) {
-            errorMessage = "The name is too short. Minimum length 2 characters";
+            errorMessage = "Minimum length " + length + " characters";
         }
-        document.getElementById("errorMessage").innerHTML = errorMessage;
+        else if (!validInput) {
+            errorMessage = "Invalid field. Some characters are not allowed";
+        }
+        var elementTarget = document.getElementById('field_'+element).getElementsByClassName("errorMessage")[0];
+        elementTarget.innerHTML = errorMessage;
     };
     document.getElementById(element).onfocus = function () {
-        document.getElementById('errorMessage').innerHTML = "";
+        var elementTarget = document.getElementById('field_'+element).getElementsByClassName("errorMessage")[0];
+        elementTarget.innerHTML = "";
     }
+}
+
+function validate(element) {
+    var regEx;
+    var id = document.getElementById(element);
+    switch (element) {
+        case "firstname":
+            regEx = /^([a-zA-Z]){1,}('|\s|-)?[a-zA-Z]{1,}$/;
+            break;
+        case "lastname":
+            regEx = /^([a-zA-Z]){1,}('|\s|-)?[a-zA-Z]{1,}$/;
+            break;
+        case "email":
+            regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,30}))$/;
+            break;
+        case "phone":
+            regEx = /^\d[0-9]{11}$/;
+            break;
+        case "zha":
+            regEx = /([zZ][hH][aA])[0-9]{6}$/;
+            break;
+        default:
+            console.log("Default case in switch statement");
+            return false;
+    }
+    return regEx.test(id.value);
 }
