a4e9ae665541f55fba10fe8bdb22569f1b0d7b14
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-06-29 00:15:59 +0100

    All the data validate. Two comments added about ZHA and phone validation. If there is the standard text, a wrong boolean may be returned.
    TODO:
    1. Validate ZHA number and phone number
    2. error messages in contactForm (part 2)
    3. add comments where necessary
    4. refactor for a better reuse?

--- Part1/contactform.js
+++ Part1/contactform.js
@@ -2,10 +2,10 @@ window.onload = init;
 
 function init() {
     setInitialFocus();
-    textHint("zha", "ZHA123456");
-    textHint("phone", "11 digits only");
+    focusBlurDefaultText("zha", "ZHA123456");
     toolTip();
     formErrorsCheck();
+    submitForm();
 }
 
 // this function simply set the focus on the box first name when the page is fully loaded
@@ -13,24 +13,20 @@ function setInitialFocus() {
     document.getElementById("firstname").focus();
 }
 
-/* function to create a placeholder with a message.
-   @param element : element to be modified
-   @param message : message to be displayed in the placeholder
-   */
-function textHint(element, defaultText) {
+/*   This function check the focus on the last name input box. Is that box is active, the placeholder message disappears.
+ *   If the user leaves the box empty, the message reappears.
+ *   @param element : element to be modified
+ *   @param message : message to be displayed in the placeholder*/
+function focusBlurDefaultText(element, defaultMessage) {
     document.getElementById(element).setAttribute("class", "defaultText");
-    document.getElementById(element).setAttribute("value", defaultText);
-    setDefaultText(element, defaultText);
-}
-
-// this function check the focus on the last name input box. Is that box is active, the placeholder message disappears.
-// If the user leaves the box empty, the message reappears.
-function setDefaultText(element, defaultMessage) {
+    document.getElementById(element).setAttribute("value", defaultMessage);
     document.getElementById(element).onfocus = function () {
+
         if (this.value === defaultMessage) {
             document.getElementById(element).setAttribute("class", "normalText");
             this.value = "";
         }
+
     };
     document.getElementById(element).onblur = function () {
         if (this.value === defaultMessage) {
@@ -60,11 +56,17 @@ function formErrorsCheck() {
     nameErrorMessage('lastname', 2);
     validEmail();
     validTitle();
+    validPhone();
+    validZha();
 }
 
 function validTitle(){
     var title = document.getElementById("title").value;
 
+    switch (title) {
+        // to do in case the return -1 doesn't work
+            // by value it takes the lowercase one
+    }
     return title != -1;
 }
 
@@ -89,10 +91,103 @@ function validEmail() {
     };
 }
 
-// function to show an error message if the input box is empty or does not meet the required length.
-/*
+function validPhone() {
+    var element = "phone";
+    var defaultMessage = "11 digits only";
 
- */
+    document.getElementById(element).setAttribute("class", "defaultText");
+    document.getElementById(element).setAttribute("value", defaultMessage);
+
+    document.getElementById(element).onfocus = function () {
+        var elementPhone = document.getElementById("field_phone").getElementsByClassName("errorMessage")[0];
+        elementPhone.innerHTML = "";
+        if (this.value === defaultMessage) {
+            document.getElementById(element).setAttribute("class", "normalText");
+            this.value = "";
+        }
+    };
+
+    document.getElementById(element).onblur = function () {
+        var phoneNumber = document.getElementById("phone").value;
+        var errorMessage = "";
+        var validPhone = validate(element);
+
+        if (this.value === defaultMessage) {
+            document.getElementById(element).setAttribute("class", "defaultText");
+        }
+        else if (this.value === "") {
+            document.getElementById(element).setAttribute("class", "defaultText");
+            this.value = defaultMessage;
+        }
+
+
+        if (!phoneNumber == "" || !validPhone) {
+            errorMessage = "Invalid number";
+        }
+        var elementPhone = document.getElementById("field_phone").getElementsByClassName("errorMessage")[0];
+        elementPhone.innerHTML = errorMessage;
+    };
+}
+
+function validZha() {
+    var element = "zha";
+    var defaultMessage = "ZHA123456";
+    document.getElementById(element).setAttribute("class", "defaultText");
+    document.getElementById(element).setAttribute("value", defaultMessage);
+
+    document.getElementById(element).onfocus = function () {
+        var elementZha = document.getElementById("field_"+element).getElementsByClassName("errorMessage")[0];
+        elementZha.innerHTML = "";
+        if (this.value === defaultMessage) {
+            document.getElementById(element).setAttribute("class", "normalText");
+            this.value = "";
+        }
+    };
+
+    document.getElementById(element).onblur = function () {
+        if (this.value === defaultMessage) {
+            document.getElementById(element).setAttribute("class", "defaultText");
+        }
+        else if (this.value === "") {
+            document.getElementById(element).setAttribute("class", "defaultText");
+            this.value = defaultMessage;
+        }
+
+        var validZha = validate(element);
+        var errorMessage = "";
+        if (!validZha) {
+            errorMessage = "Invalid number. It starts with \'ZHA\' followed by 6 numbers";
+        }
+        var elementZha = document.getElementById("field_"+element).getElementsByClassName("errorMessage")[0];
+        elementZha.innerHTML = errorMessage;
+    };
+}
+
+function submitForm(){
+    document.getElementById("submit").onclick = function () {
+        var firstname = validate("firstname");
+        var lastname = validate("lastname");
+        var title = validTitle();
+        var validZha = validate("zha");
+        var email = validate("email");
+        var phone = validate("phone") || document.getElementById("phone").value === 0;
+        var collectionOfErrors = [firstname,
+                lastname,
+                title,
+                validZha,   // if default text is there, it returns true;
+                email,
+                phone       // if default text is there, it returns true;
+            ];
+        console.log(collectionOfErrors.toString());
+        if (collectionOfErrors.includes(false)){
+            alert("error in the form\n" +
+                collectionOfErrors.toString());
+        }
+        return false;
+    }
+}
+
+// function to show an error message if the input box is empty or does not meet the required length.
 function nameErrorMessage(element, length){
     document.getElementById(element).onblur = function() {
         var elementValue = document.getElementById(element).value;
@@ -131,7 +226,7 @@ function validate(element) {
             regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,30}))$/;
             break;
         case "phone":
-            regEx = /^\d[0-9]{11}$/;
+            regEx = /^\d[0-9]{10}$/;
             break;
         case "zha":
             regEx = /([zZ][hH][aA])[0-9]{6}$/;
